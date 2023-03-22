51ad5c4c9d9ed6847bb3ec2fcdffc7e190dcad3a
Nadiia Onopriienko <nadiia@ok-shortcut.local>
2023-03-22 21:44:42 +0200

    update backend url

--- src/api/transactionApi.js
+++ src/api/transactionApi.js
@@ -4,7 +4,7 @@ export const transactionApi = {
     getTransactionData: async (data) => {
         return axios({
             method: 'get',
-            url: `http://localhost:8080/api/transaction_info`,
+            url: `https://test-full-stack-dev-nadinok.onrender.com/api/transaction_info`,
             params: data,
             headers: {
                 'Content-Type': 'application/json'
